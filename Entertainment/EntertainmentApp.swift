fffab19548ca3b493204bb920525c47a37900b40
James-SwiftUI <jamesholmson@gmail.com>
2024-01-13 13:57:57 +0000

    Initial Commit

---
+++ Entertainment/EntertainmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  EntertainmentApp.swift
+//  Entertainment
+//
+//  Created by James on 13/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct EntertainmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
